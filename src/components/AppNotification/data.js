089fbaaff2623a2370513ff0f4f99a34172d19b9
martintorok <martin.torok@gmail.com>
2018-03-27 16:38:02 +1100

    Notifications.

--- src/components/AppNotification/data.js
+++ src/components/AppNotification/data.js
@@ -1,29 +1,15 @@
 export const notifications = [
-
     {
-        image: 'http://via.placeholder.com/120x120',
-        title: "Stella Johnson has recently posted an album",
+        title: "Your%20workload%20%3Cb%3EWorkload%20547%3C/b%3E%20has%20started."
         time: "4:10 PM",
-        icon: "zmdi-thumb-up text-blue",
+        date: "2018-03-27",
     }, {
-        image: 'http://via.placeholder.com/150x150',
-        title: "Alex Brown has shared Martin Guptil's post",
+        title: "Your%20workload%20%3Cb%3EWorkload%20547%3C/b%3E%20has%20been%20interrupted%20as%20price%20has%20increased%20to%203.6%20CRC/hr.%20Raise%20your%20max%20price%20to%20continue%20execution.",
         time: "5:18 PM",
-        icon: "zmdi-comment-text text-grey",
+        date: "2018-03-26",
     }, {
-        image: 'http://via.placeholder.com/210x210',
-        title: "Domnic Brown has sent you a group invitation for Global Health",
+        title: "Price%20increased%20for%20%3Cb%3EWorkload%20547%3C/b%3E%20from%202.5%20CRC/hr%20to%202.8%20CRC/hr",
         time: "5:36 PM",
-        icon: "zmdi-card-giftcard text-info",
-    }, {
-        image: 'http://via.placeholder.com/210x210',
-        title: "John Smith has birthday today",
-        time: "5:54 PM",
-        icon: "zmdi-cake text-warning",
-    },{
-        image: 'http://via.placeholder.com/150x150',
-        title: "Chris has updated his profile picture",
-        time: "5:25 PM",
-        icon: "zmdi-account-box-o text-grey",
-    }
+        date: "2018-03-25",
+    },
 ];
