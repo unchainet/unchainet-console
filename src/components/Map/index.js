ecd148840c70de361502bcd948ec9cd83debb944
martintorok <martin.torok@gmail.com>
2018-03-31 01:04:57 +1100

    styling

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import {
-  withScriptjs,
   withGoogleMap,
   GoogleMap
 } from 'react-google-maps';
 
-const Map = withScriptjs(withGoogleMap(props =>
+const mapStyles = require('../../mapStyles.json');
+
+const Map = withGoogleMap(props =>
   <GoogleMap
     defaultZoom={8}
     defaultCenter={{ lat: -34.397, lng: 150.644 }}
-
+    defaultOptions={{
+      styles: mapStyles
+    }}
     {...props}
   >
     {props.children}
   </GoogleMap>
-));
+);
 
 export default Map;
